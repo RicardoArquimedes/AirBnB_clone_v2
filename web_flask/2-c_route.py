1a02ee2814ddb64f40567997a235268510250570
RicardoArquimedes <richardcqt28@gmail.com>
2021-01-19 19:07:26 -0500

    adding task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, request
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ hello function """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    """ HBNB function """
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_route(text):
+    """ Routing with a string as dynamic parameter """
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
